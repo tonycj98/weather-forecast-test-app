23cad909e95644dc7ecd8aeb987aa29ea0861817
Tony James <64031592+tonycj98@users.noreply.github.com>
2024-10-26 06:19:12 +0000

    Refactor WeatherForecast component to handle weather data fetching and error handling

--- frontend/src/components/weather-forecast.component.ts
+++ frontend/src/components/weather-forecast.component.ts
@@ -1,4 +1,6 @@
-import WeatherService from "@/services/weather-service.service";
+import WeatherService, {
+  ForecastModel,
+} from "@/services/weather-service.service";
 import { Options, Vue } from "vue-class-component";
 import { Inject, Prop, Watch } from "vue-property-decorator";
 import WeatherCard from "@/components/WeatherCard.vue";
@@ -18,6 +20,12 @@ export default class WeatherForecast extends Vue {
   @Prop()
   place!: { lat: number; lng: number } | null;
 
+  weatherData: {
+    data: ForecastModel | null;
+    error: boolean;
+    loading: boolean;
+  } = { data: null, error: false, loading: false };
+
   /*
   mounted() {
     // TODO - use the latitude and longitude from the search city component
@@ -31,19 +39,35 @@ export default class WeatherForecast extends Vue {
 
   @Watch("place", { immediate: true, deep: true })
   onPlaceChanged(newPlace: { lat: number; lng: number }) {
-    if (newPlace) {
-      this.fetchWeather(newPlace);
+    if (
+      typeof newPlace?.lat != "number" ||
+      typeof newPlace?.lng != "number" ||
+      Number.isNaN(newPlace?.lat) ||
+      Number.isNaN(newPlace?.lng)
+    ) {
+      console.error("lat or lng not a number", newPlace);
+      this.weatherData.error = true;
+      return;
     }
+    this.fetchWeather(newPlace);
   }
 
   fetchWeather({ lat, lng }: { lat: number; lng: number }) {
+    this.weatherData.loading = true;
+    this.weatherData.error = false;
     this.weatherService
       .getWeatherForecast(lat, lng)
       .then((res) => {
         console.log(res);
+        // TODO: Error Handling
+        this.weatherData.data = res;
       })
       .catch((error) => {
-        console.error(error);
+        console.error("Error in weatherService.getWeatherForecast", error);
+        this.weatherData.error = true;
+      })
+      .finally(() => {
+        this.weatherData.loading = false;
       });
   }
 }
